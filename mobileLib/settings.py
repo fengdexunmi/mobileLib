ea31540dd54bb28424c60e6173488cf952d58440
fengdexunmi <fang.oscar07@gmail.com>
2014-06-22 00:31:07 +0800

    Update settings.py

--- mobileLib/settings.py
+++ mobileLib/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jq(yws3-4j&)lgix!tni!-3&0ik2$3)$ub@-9!kztdofs$0au1'
+SECRET_KEY = 'Your KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
